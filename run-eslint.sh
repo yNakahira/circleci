1853a69ec39e652aeac2823a99ec894009738f6d
yusuke nakahira <nakahira.yusuke@teamspirit.co.jp>
2017-02-09 00:46:13 +0900

    shでエラーになっていた個所を修正

--- run-eslint.sh
+++ run-eslint.sh
@@ -7,19 +7,6 @@ if [ "${CIRCLE_BRANCH}" != "master" ]; then
   gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
 
   echo "********************"
-  echo "* save outputs     *"
-  echo "********************"
-
-  LINT_RESULT_DIR="$CIRCLE_ARTIFACTS/lint"
-
-  mkdir "$LINT_RESULT_DIR"
-  cp -v "app/build/reports/checkstyle/checkstyle.xml" "$LINT_RESULT_DIR/"
-  cp -v "app/build/reports/findbugs/findbugs.xml" "$LINT_RESULT_DIR/"
-  cp -v "app/build/reports/pmd/pmd.xml" "$LINT_RESULT_DIR/"
-  cp -v "app/build/reports/pmd/cpd.xml" "$LINT_RESULT_DIR/"
-  cp -v "app/build/outputs/lint-results.xml" "$LINT_RESULT_DIR/"
-
-  echo "********************"
   echo "* select reporter  *"
   echo "********************"
 
@@ -33,7 +20,7 @@ if [ "${CIRCLE_BRANCH}" != "master" ]; then
   echo saddler
   git diff --name-only origin/master \
    | grep '.*\.js$' \
-   | xargs node_modules/eslint/bin/eslint.js -f node_modules/eslint/lib/formatters/checkstyle.js \
+   | xargs node_modules/eslint/bin/eslint.js -f checkstyle \
    | checkstyle_filter-git diff origin/master \
    | saddler report --require saddler/reporter/github --reporter $REPORTER
 
