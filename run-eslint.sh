f6c0c85457ada2df5101d57bd0d633f9912ba0a9
yusuke nakahira <nakahira.yusuke@teamspirit.co.jp>
2017-02-09 00:04:47 +0900

    checkstyle指定方法を修正

--- run-eslint.sh
+++ run-eslint.sh
@@ -16,18 +16,18 @@ if [ "${CIRCLE_BRANCH}" != "master" ]; then
   echo eslint
   git diff --name-only origin/master \
    | grep '.*\.js$' \
-   | xargs node_modules/eslint/bin/eslint.js -f node_modules/eslint/lib/formatters/checkstyle \
+   | xargs node_modules/eslint/bin/eslint.js -f node_modules/eslint/lib/formatters/checkstyle.js \
 
   echo checkstyle_filter-git
   git diff --name-only origin/master \
    | grep '.*\.js$' \
-   | xargs node_modules/eslint/bin/eslint.js -f node_modules/eslint/lib/formatters/checkstyle \
+   | xargs node_modules/eslint/bin/eslint.js -f node_modules/eslint/lib/formatters/checkstyle.js \
    | checkstyle_filter-git diff origin/master
 
   echo saddler
   git diff --name-only origin/master \
    | grep '.*\.js$' \
-   | xargs node_modules/eslint/bin/eslint.js -f node_modules/eslint/lib/formatters/checkstyle \
+   | xargs node_modules/eslint/bin/eslint.js -f node_modules/eslint/lib/formatters/checkstyle.js \
    | checkstyle_filter-git diff origin/master \
    | saddler report \
       --require saddler/reporter/github \
