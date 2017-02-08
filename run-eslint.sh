70dad356e9f0f5e0cae3859401147174ac83f9b1
yusuke nakahira <nakahira.yusuke@teamspirit.co.jp>
2017-02-09 00:13:22 +0900

    不要な処理を削除

--- run-eslint.sh
+++ run-eslint.sh
@@ -6,24 +6,6 @@ if [ "${CIRCLE_BRANCH}" != "master" ]; then
   echo gem install
   gem install --no-document checkstyle_filter-git saddler saddler-reporter-github
 
-  echo git diff
-  git diff --name-only origin/master
-
-  echo file grep
-  git diff --name-only origin/master \
-   | grep '.*\.js$'
-
-  echo eslint
-  git diff --name-only origin/master \
-   | grep '.*\.js$' \
-   | xargs node_modules/eslint/bin/eslint.js -f node_modules/eslint/lib/formatters/checkstyle.js \
-
-  echo checkstyle_filter-git
-  git diff --name-only origin/master \
-   | grep '.*\.js$' \
-   | xargs node_modules/eslint/bin/eslint.js -f node_modules/eslint/lib/formatters/checkstyle.js \
-   | checkstyle_filter-git diff origin/master
-
   echo saddler
   git diff --name-only origin/master \
    | grep '.*\.js$' \
